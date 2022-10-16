08c5d8ecc549d097b5283d42d5c2585a4d9a84b7
Bryan Emerson <bryan.l.emerson@gmail.com>
2022-10-16 16:13:06 -0700

    eslint init

--- app.js
+++ app.js
@@ -1,14 +1,13 @@
-let express = require('express');
+const express = require('express');
 
-let app = express();
+const app = express();
 
-let port = process.env.port || 3000;
+const port = process.env.port || 3000;
 
-
-app.get('/', (req, res)=> {
-  res.send('Welcome Friends! This is my API!')
+app.get('/', (req, res) => {
+  res.send('Welcome Friends! This is my API!');
 });
 
-app.listen(port, ()=> {
-  console.log('Running on port ' + port);
+app.listen(port, () => {
+  console.log(`Running on port ${port}`);
 });
