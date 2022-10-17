29eeceba79f290019fe62ee8334633e9cad17e6e
Bryan Emerson <bryan.l.emerson@gmail.com>
2022-10-17 13:45:54 -0700

    get by id and query string instantiated but query str not working

--- app.js
+++ app.js
@@ -4,16 +4,29 @@ const mongoose = require('mongoose');
 const app = express();
 const db = mongoose.connect('mongodb://localhost/bookAPI');
 const bookRouter = express.Router();
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 const Book = require('./models/bookModel');
 
 bookRouter.route('/books')
   .get((req, res) => {
-    Book.find((err, books) => {
+    const query = {};
+    if (req.query.genre) {
+      query.genre = req.query.genre;
+    }
+    Book.find(query, (err, books) => {
       if (err) {
         return res.send(err);
       }
       return res.json(books);
+    });
+  });
+bookRouter.route('/books/:bookId')
+  .get((req, res) => {
+    Book.findById(req.params.bookId, (err, book) => {
+      if (err) {
+        return res.send(err);
+      }
+      return res.json(book);
     });
   });
 
